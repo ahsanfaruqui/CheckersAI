9551f07b7ecb6713121e1f644e217f6ee8ba824d
Ghalib <ahsan.faruqui198@gmail.com>
2021-03-29 00:58:19 +0500

    Milestone 3

---
+++ checkers/constants.py
@@ -0,0 +1,13 @@
+import pygame
+
+WIDTH, HEIGHT = 600, 600
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+RED = (0, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (255,192,203)
+
---
+++ main.py
@@ -0,0 +1,42 @@
+
+import pygame
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
+from checkers.board import Board
+from checkers.piece import Piece
+
+#vfgtt
+
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    board=Board()
+
+
+    while run:
+        clock.tick(FPS)
+
+
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+        board.draw(WIN)
+        pygame.display.update()
+    
+    pygame.quit()
+
+main()
