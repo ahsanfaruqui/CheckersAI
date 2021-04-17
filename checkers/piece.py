76503aa74bea963713caf887f12255f586122425
Ahsan <ahsan.faruqui198@gmail.com>
2021-04-18 02:23:33 +0500

    final

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,9 +1,9 @@
-from .constants import RED, WHITE, SQUARE_SIZE, GREY
+from .constants import RED, WHITE, SQUARE_SIZE, GREY, CROWN
 import pygame
 
 class Piece:
-    PADDING = 20
-    OUTLINE = 10
+    PADDING = 15
+    OUTLINE = 2
 
     def __init__(self, row, col, color):
         self.row = row
@@ -25,5 +25,13 @@ class Piece:
         radius = SQUARE_SIZE//2 - self.PADDING
         pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
 
+    def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.calc_pos()
 
+    def __repr__(self):
+        return str(self.color)
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-
+# Assets: https://techwithtim.net/wp-content/uploads/2020/09/assets.zip
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
-from checkers.board import Board
-from checkers.piece import Piece
 
-#AI
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
+from checkers.game import Game
+from minimax.algorithm import minimax
 
 FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
+
 def get_row_col_from_mouse(pos):
     x, y = pos
     row = y // SQUARE_SIZE
@@ -19,23 +19,29 @@ def get_row_col_from_mouse(pos):
 def main():
     run = True
     clock = pygame.time.Clock()
-    board=Board()
-
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
+        
+        if game.turn == WHITE:
+            value, new_board = minimax(game.get_board(), 4, WHITE, game)
+            game.ai_move(new_board)
 
-
-
+        if game.winner() != None:
+            print(game.winner())
+            run = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-        board.draw(WIN)
-        pygame.display.update()
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
     
     pygame.quit()
 
